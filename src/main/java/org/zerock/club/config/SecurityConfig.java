00582d7d031003733767d91be84b7954fab04a31
kangckck24@gmail.com <kangckck24@gmail.com>
2022-07-21 00:41:20 +0900

    코드로배우는 스프링부트 12장 2.7.1 버전

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,8 +15,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.zerock.club.security.filter.ApiCheckFilter;
+import org.zerock.club.security.filter.ApiLoginFilter;
+import org.zerock.club.security.handler.ApiLoginFailHandler;
 import org.zerock.club.security.handler.ClubLoginSuccessHandler;
 import org.zerock.club.security.service.ClubUserDetailsService;
+import org.zerock.club.security.util.JWTUtil;
 
 @Configuration
 @Log4j2
@@ -29,28 +36,17 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsService() {
-//        UserDetails user = User.builder()
-//                .username("user1")
-//                .password(passwordEncoder().encode("1111"))
-//                .roles("USER")
-//                .build();
-//
-//        log.info("userDetailsService............................");
-//        log.info(user);
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //AuthenticationManager설정
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        // Get AuthenticationManager
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
-//        http.authorizeHttpRequests((auth) -> {
-//            auth.antMatchers("/sample/all").permitAll();
-//            auth.antMatchers("/sample/member").hasRole("USER");
-//        });
+        //반드시 필요
+        http.authenticationManager(authenticationManager);
 
         http.formLogin();
         http.csrf().disable();
@@ -58,12 +54,50 @@ public class SecurityConfig {
         http.oauth2Login().successHandler(successHandler());
 
         http.rememberMe().tokenValiditySeconds(60*60*24*7).userDetailsService(userDetailsService);
+        http.addFilterBefore(apiCheckFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        http.addFilterBefore(apiLoginFilter(authenticationManager), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    public ApiLoginFilter apiLoginFilter(AuthenticationManager authenticationManager) throws Exception{
+
+        ApiLoginFilter apiLoginFilter =  new ApiLoginFilter("/api/login", jwtUtil());
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        apiLoginFilter
+                .setAuthenticationFailureHandler(new ApiLoginFailHandler());
+
+        return apiLoginFilter;
+    }
+
+
+
+    @Bean
+    public JWTUtil jwtUtil() {
+        return new JWTUtil();
     }
 
     @Bean
     public ClubLoginSuccessHandler successHandler() {
         return new ClubLoginSuccessHandler(passwordEncoder());
     }
+
+    @Bean
+    public ApiCheckFilter apiCheckFilter() {
+
+        return new ApiCheckFilter("/notes/**/*", jwtUtil());
+    }
+
+//    public ApiLoginFilter apiLoginFilter(AuthenticationManager authenticationManager) throws Exception{
+//
+//        ApiLoginFilter apiLoginFilter =  new ApiLoginFilter("/api/login", jwtUtil());
+//        apiLoginFilter.setAuthenticationManager(authenticationManager);
+//
+//        return apiLoginFilter;
+//    }
+
+
+
 }
